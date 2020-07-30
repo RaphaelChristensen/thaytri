ea6f2ea1cc8bf51ef48e0bda5d760d3d52049f6c
Raphael Christensen <1656100058@hcmussh.edu.vn>
2020-07-30 23:27:36 +0700

    Giai Apriori thay Tri

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Bài tập Khai thác Dữ liệu / Thầy Trí
 
+- bt1_1.java : Bài giải câu hỏi 1 - BT1_KTDL_K33
+- bt1_2.java : Bài giải câu hỏi 2 - BT1_KTDL_K33
+
--- project_1.java
+++ project_1.java
@@ -1,253 +1,267 @@
 import java.awt.SystemTray;
 
-//Giải bài tập Apriori 2 - môn Khai thác Dữ liệu
+//Giải bài tập Đồ án_Bài tập 1 bằng Apriori - môn Khai thác Dữ liệu
 
-class bt1_2 {
+class project_1 {
 	public static void main(String[] args) {
 		//itemsetset
-		String[] itemset = {"", "M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11"};
+		String[] itemset = {"", "I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10", "I11"};
 
 		int dem = 0; //Kiểm tra tổng số tập phổ biến nhằm tiếp tục hoặc kết thúc bài toán
 		// ===================================== I N S E R T == V A L U E ======
-            //$$$$$$$$$$$$$$$ Nhập tổng số lượng các hạng mục $$$$$$$$$$$
-            int item = 11;
-            int item_tab = item + 1; //Need null value so as to sync easily in arrays
+						//$$$$$$$$$$$$$$$ Nhập tổng số lượng các hạng mục $$$$$$$$$$$
+						int item = 11;
+						int item_tab = item + 1; //Need null value so as to sync easily in arrays
 
-            //$$$$$$$$$$$$$$$ Nhập lượng giao dịch $$$$$$$$$$$$$
-            int tid = 35;
-            int tid_tab = tid + 1; ////Need null value so as to sync easily in arrays
+						//$$$$$$$$$$$$$$$ Nhập lượng giao dịch $$$$$$$$$$$$$
+						int tid = 39;
+						int tid_tab = tid + 1; ////Need null value so as to sync easily in arrays
 
-            //$$$$$$$$$$$$$$$ Nhập độ phổ biến (đơn vị %) $$$$$$$$$$$$$
-            int minsupp = 20;
-            int minsupp_count = minsupp*tid/100;
-            System.out.println("===============||| Số đếm độ tin cậy = " + minsupp_count + "||| ===============");
-            //$$$$$$$$$$$$$$$ Nhập độ tin cậy (đơn vị %) $$$$$$$$$$$$$
-            int minconf = 50;
-            float min_conf_pt = minconf/100;
+						//$$$$$$$$$$$$$$$ Nhập độ phổ biến (đơn vị %) $$$$$$$$$$$$$
+						int minsupp = 20;
+						int minsupp_count = minsupp*tid/100;
+						System.out.println("===============||| Supp_count = " + minsupp_count + "||| ===============");
+						//$$$$$$$$$$$$$$$ Nhập độ tin cậy (đơn vị %) $$$$$$$$$$$$$
+						int minconf = 30;
+						float min_conf_pt = minconf/100;
 
-            // ===================================== I N S E R T == V A L U E ======
+						// ===================================== I N S E R T == V A L U E ======
 
-            //Hàng : số giao dịch, cột : các đối tượng
-            int tab[][] = new int[tid_tab][item_tab];
+						//Hàng : số giao dịch, cột : các đối tượng
+						int tab[][] = new int[tid_tab][item_tab];
 
 
-		for(int ox = 0 ; ox < tid_tab ; ox++ ) {
-			for(int oy = 0 ; oy < item_tab ; oy++) {
-				if(oy == 0) {
-					tab[ox][oy] = ox;
-				} else if(ox == 0) {
-					tab[ox][oy] = oy;
+		for(int row = 0 ; row < tid_tab ; row++ ) {
+			for(int col = 0 ; col < item_tab ; col++) {
+				if(col == 0) {
+					tab[row][col] = row;
+				} else if(row == 0) {
+					tab[row][col] = col;
 				} else {
-					tab[ox][oy] = 0;
+					tab[row][col] = 0;
 				}
 			}
 		}
 		//insert value
 
-		tab[1][3] = 1;
-		tab[1][1] = 1;
-		tab[1][4] = 1;
-		tab[1][2] = 1;
-		tab[1][6] = 1;
-		tab[1][11] = 1;
+				tab[1][10] = 1;
+				tab[1][3] = 1;
+				tab[1][1] = 1;
+				tab[1][4] = 1;
+				tab[1][2] = 1;
+				tab[1][6] = 1;
+				tab[1][11] = 1;
 
-		tab[2][5] = 1;
-		tab[2][3] = 1;
-		tab[2][1] = 1;
-		tab[2][4] = 1;
-		tab[2][6] = 1;
-		tab[2][7] = 1;
-		tab[2][8] = 1;
-		tab[2][9] = 1;
+				tab[2][6] = 1;
+				tab[2][3] = 1;
+				tab[2][1] = 1;
+				tab[2][7] = 1;
+				tab[2][8] = 1;
+				tab[2][9] = 1;
 
-		tab[3][6] = 1;
-		tab[3][8] = 1;
-		tab[3][1] = 1;
-		tab[3][5] = 1;
-		tab[3][10] = 1;
+				tab[3][5] = 1;
+				tab[3][8] = 1;
+				tab[3][1] = 1;
+				tab[3][6] = 1;
+				tab[3][10] = 1;
 
-		tab[4][2] = 1;
+				tab[4][3] = 1;
 
-		tab[5][3] = 1;
-		tab[5][4] = 1;
+				tab[5][10] = 1;
 
-		tab[6][1] = 1;
-		tab[6][2] = 1;
-		tab[6][3] = 1;
-		tab[6][5] = 1;
-		tab[6][6] = 1;
-		tab[6][7] = 1;
-		tab[6][8] = 1;
-		tab[6][9] = 1;
+				tab[6][2] = 1;
+				tab[6][3] = 1;
+				tab[6][5] = 1;
+				tab[6][6] = 1;
+				tab[6][7] = 1;
+				tab[6][8] = 1;
+				tab[6][9] = 1;
 
-		tab[7][2] = 1;
-		tab[7][4] = 1;
-		tab[7][6] = 1;
-		tab[7][7] = 1;
-		tab[7][8] = 1;
+				tab[7][2] = 1;
+				tab[7][4] = 1;
+				tab[7][6] = 1;
+				tab[7][7] = 1;
 
-		tab[8][2] = 1;
-		tab[8][1] = 1;
-		tab[8][3] = 1;
-		tab[8][4] = 1;
-		tab[8][5] = 1;
-		tab[8][6] = 1;
-		tab[8][7] = 1;
-		tab[8][8] = 1;
+				tab[8][2] = 1;
+				tab[8][1] = 1;
+				tab[8][3] = 1;
+				tab[8][4] = 1;
+				tab[8][5] = 1;
+				tab[8][6] = 1;
+				tab[8][8] = 1;
 
-		tab[9][6] = 1;
-		tab[9][8] = 1;
-		tab[9][5] = 1;
-		tab[9][1] = 1;
-		tab[9][4] = 1;
-		tab[9][3] = 1;
-		tab[9][7] = 1;
-		tab[9][2] = 1;
+				tab[9][6] = 1;
+				tab[9][8] = 1;
+				tab[9][1] = 1;
+				tab[9][4] = 1;
+				tab[9][3] = 1;
+				tab[9][2] = 1;
 
-		tab[10][5] = 1;
-		tab[10][1] = 1;
-		tab[10][4] = 1;
-		tab[10][3] = 1;
-		tab[10][7] = 1;
-		tab[10][10] = 1;
+				tab[10][5] = 1;
+				tab[10][1] = 1;
+				tab[10][4] = 1;
+				tab[10][3] = 1;
+				tab[10][7] = 1;
+				tab[10][10] = 1;
 
-		tab[11][6] = 1;
-		tab[11][1] = 1;
-		tab[11][4] = 1;
-		tab[11][2] = 1;
-		tab[11][8] = 1;
+				tab[11][6] = 1;
+				tab[11][1] = 1;
+				tab[11][4] = 1;
+				tab[11][2] = 1;
+				tab[11][8] = 1;
 
-		tab[12][7] = 1;
-		tab[12][8] = 1;
-		tab[12][2] = 1;
+				tab[12][8] = 1;
+				tab[12][1] = 1;
+				tab[12][4] = 1;
 
-		tab[13][1] = 1;
-		tab[13][3] = 1;
-		tab[13][5] = 1;
-		tab[13][7] = 1;
+				tab[13][1] = 1;
+				tab[13][3] = 1;
+				tab[13][5] = 1;
+				tab[13][7] = 1;
 
-		tab[14][2] = 1;
-		tab[14][4] = 1;
-		tab[14][6] = 1;
-		tab[14][8] = 1;
-		tab[14][11] = 1;
+				tab[14][2] = 1;
+				tab[14][4] = 1;
+				tab[14][6] = 1;
+				tab[14][8] = 1;
+				tab[14][11] = 1;
 
-		tab[15][1] = 1;
-		tab[15][3] = 1;
-		tab[15][6] = 1;
-		tab[15][7] = 1;
+				tab[15][1] = 1;
+				tab[15][3] = 1;
+				tab[15][6] = 1;
+				tab[15][7] = 1;
 
-		tab[16][4] = 1;
-		tab[16][2] = 1;
-		tab[16][8] = 1;
-		tab[16][5] = 1;
-		tab[16][3] = 1;
+				tab[16][4] = 1;
+				tab[16][2] = 1;
+				tab[16][8] = 1;
+				tab[16][5] = 1;
+				tab[16][3] = 1;
 
-		tab[17][2] = 1;
-		tab[17][4] = 1;
-		tab[17][6] = 1;
-		tab[17][7] = 1;
+				tab[17][2] = 1;
+				tab[17][4] = 1;
+				tab[17][6] = 1;
+				tab[17][7] = 1;
+				tab[17][9] = 1;
 
-		tab[18][1] = 1;
-		tab[18][2] = 1;
-		tab[18][3] = 1;
-		tab[18][5] = 1;
-		tab[18][6] = 1;
-		tab[18][7] = 1;
-		tab[18][8] = 1;
-		tab[18][10] = 1;
-		tab[18][11] = 1;
+				tab[18][1] = 1;
+				tab[18][2] = 1;
+				tab[18][3] = 1;
+				tab[18][5] = 1;
+				tab[18][7] = 1;
+				tab[18][8] = 1;
+				tab[18][10] = 1;
+				tab[18][11] = 1;
 
-		tab[19][1] = 1;
-		tab[19][4] = 1;
-		tab[19][3] = 1;
-		tab[19][5] = 1;
-		tab[19][6] = 1;
+				tab[19][1] = 1;
+				tab[19][4] = 1;
+				tab[19][3] = 1;
+				tab[19][5] = 1;
+				tab[19][6] = 1;
 
-		tab[20][1] = 1;
-		tab[20][2] = 1;
-		tab[20][7] = 1;
+				tab[20][1] = 1;
+				tab[20][2] = 1;
+				tab[20][7] = 1;
 
-		tab[21][8] = 1;
-		tab[21][4] = 1;
-		tab[21][3] = 1;
-		tab[21][1] = 1;
-		tab[21][5] = 1;
-		tab[21][9] = 1;
+				tab[21][8] = 1;
+				tab[21][4] = 1;
+				tab[21][3] = 1;
+				tab[21][1] = 1;
+				tab[21][5] = 1;
+				tab[21][9] = 1;
 
-		tab[22][5] = 1;
-		tab[22][2] = 1;
+				tab[22][5] = 1;
+				tab[22][2] = 1;
+				tab[22][10] = 1;
 
-		tab[23][3] = 1;
+				tab[23][3] = 1;
+				tab[23][4] = 1;
 
-		tab[24][8] = 1;
-		tab[24][1] = 1;
-		tab[24][4] = 1;
+				tab[24][7] = 1;
+				tab[24][8] = 1;
+				tab[24][2] = 1;
+				tab[24][3] = 1;
 
-		tab[25][2] = 1;
-		tab[25][5] = 1;
-		tab[25][6] = 1;
-		tab[25][8] = 1;
-		tab[25][4] = 1;
-		tab[25][1] = 1;
+				tab[25][2] = 1;
+				tab[25][5] = 1;
+				tab[25][6] = 1;
+				tab[25][8] = 1;
+				tab[25][4] = 1;
+				tab[25][1] = 1;
 
-		tab[26][2] = 1;
-		tab[26][5] = 1;
-		tab[26][8] = 1;
+				tab[26][2] = 1;
+				tab[26][5] = 1;
+				tab[26][8] = 1;
+				tab[26][11] = 1;
 
-		tab[27][2] = 1;
-		tab[27][3] = 1;
-		tab[27][4] = 1;
-		tab[27][8] = 1;
-		tab[27][1] = 1;
+				tab[27][2] = 1;
+				tab[27][3] = 1;
+				tab[27][1] = 1;
+				tab[27][8] = 1;
+				tab[27][6] = 1;
 
-		tab[28][6] = 1;
-		tab[28][8] = 1;
-		tab[28][1] = 1;
-		tab[28][5] = 1;
-		tab[28][4] = 1;
-		tab[28][3] = 1;
-		tab[28][7] = 1;
-		tab[28][2] = 1;
+				tab[28][6] = 1;
+				tab[28][8] = 1;
+				tab[28][1] = 1;
+				tab[28][5] = 1;
+				tab[28][4] = 1;
+				tab[28][3] = 1;
+				tab[28][7] = 1;
+				tab[28][2] = 1;
 
-		tab[29][1] = 1;
+				tab[29][11] = 1;
 
-		tab[30][4] = 1;
-		tab[30][5] = 1;
-		tab[30][6] = 1;
+				tab[30][4] = 1;
+				tab[30][5] = 1;
+				tab[30][6] = 1;
 
-		tab[31][2] = 1;
-		tab[31][8] = 1;
-		tab[31][6] = 1;
-		tab[31][1] = 1;
-		tab[31][9] = 1;
+				tab[31][2] = 1;
+				tab[31][8] = 1;
+				tab[31][6] = 1;
+				tab[31][1] = 1;
+				tab[31][9] = 1;
 
-		tab[32][4] = 1;
-		tab[32][8] = 1;
-		tab[32][6] = 1;
-		tab[32][2] = 1;
+				tab[32][4] = 1;
+				tab[32][8] = 1;
+				tab[32][6] = 1;
+				tab[32][2] = 1;
 
-		tab[33][6] = 1;
-		tab[33][1] = 1;
-		tab[33][5] = 1;
-		tab[33][3] = 1;
-		tab[33][4] = 1;
+				tab[33][6] = 1;
+				tab[33][1] = 1;
+				tab[33][5] = 1;
+				tab[33][3] = 1;
+				tab[33][4] = 1;
 
-		tab[34][1] = 1;
-		tab[34][2] = 1;
-		tab[34][3] = 1;
-		tab[34][6] = 1;
-		tab[34][7] = 1;
-		tab[34][8] = 1;
+				tab[34][1] = 1;
+				tab[34][2] = 1;
+				tab[34][3] = 1;
+				tab[34][6] = 1;
+				tab[34][7] = 1;
+				tab[34][8] = 1;
 
-		tab[35][4] = 1;
-		tab[35][2] = 1;
-		tab[35][3] = 1;
-		tab[35][6] = 1;
-		tab[35][5] = 1;
-		tab[35][7] = 1;
-		tab[35][8] = 1;
-		tab[35][11] = 1;
+				tab[35][4] = 1;
+				tab[35][2] = 1;
+				tab[35][3] = 1;
+				tab[35][5] = 1;
+				tab[35][7] = 1;
+				tab[35][8] = 1;
+				tab[35][11] = 1;
+
+				tab[36][5] = 1;
+				tab[36][2] = 1;
+
+				tab[37][3] = 1;
+				tab[37][8] = 1;
+				tab[37][9] = 1;
+				tab[37][6] = 1;
+
+				tab[38][1] = 1;
+
+				tab[39][11] = 1;
+				tab[39][6] = 1;
+				tab[39][5] = 1;
+				tab[39][2] = 1;
+				tab[39][4] = 1;
+				tab[39][3] = 1;
+				tab[39][1] = 1;
 
 		//print to check
 		System.out.println("============||| Table |||================");
@@ -437,28 +451,30 @@ class bt1_2 {
 	                }
 	        }
 			//=================================Counting L 4 ==========================
-	        for(int oz = 1 ; oz < item_tab ; oz++) {
-	        	for(int oy = 1 ; oy < item_tab ; oy++) {
-	        		if(l2[oy][oz] < minsupp_count || l2 [oz][oy] < minsupp_count) continue;
-	        		else if(oy == oz) continue;
-	        		else {
-	                    for(int ox = 1 ; ox < item_tab ; ox++) {
-	    	                if(ox == oy || ox == oz) continue;
-	                        else if(l2[ox][oy]<minsupp_count || l2[oy][ox]<minsupp_count || l2[ox][oz]<minsupp_count || l2[oz][ox]<minsupp_count || l2[oy][oz]<minsupp_count || l2[oz][oy]<minsupp_count) continue;
-	                        else {
-	                            int ox_num = 1;
-	                            while(ox_num < tid_tab) {
-	                               if(tab[ox_num][ox] == 1 && tab[ox_num][oy] == 1 && tab[ox_num][oz] == 1) {
-	                                   l3[ox][oy][oz]++;
-	                               }
-	                               ox_num++;
-	                            }
-	                        }
-	                    }
-	        		}
-	        	}
-	        }
-
+					for(int ot = 1 ; ot < item_tab ; ot++) {
+						for(int oz = 1 ; oz < item_tab ; oz++) {
+							if()
+		        	for(int oy = 1 ; oy < item_tab ; oy++) {
+		        		if(l2[oy][oz] < minsupp_count || l2 [oz][oy] < minsupp_count) continue;
+		        		else if(oy == oz) continue;
+		        		else {
+		                    for(int ox = 1 ; ox < item_tab ; ox++) {
+		    	                if(ox == oy || ox == oz) continue;
+		                        else if(l2[ox][oy]<minsupp_count || l2[oy][ox]<minsupp_count || l2[ox][oz]<minsupp_count || l2[oz][ox]<minsupp_count || l2[oy][oz]<minsupp_count || l2[oz][oy]<minsupp_count) continue;
+		                        else {
+		                            int ox_num = 1;
+		                            while(ox_num < tid_tab) {
+		                               if(tab[ox_num][ox] == 1 && tab[ox_num][oy] == 1 && tab[ox_num][oz] == 1) {
+		                                   l3[ox][oy][oz]++;
+		                               }
+		                               ox_num++;
+		                            }
+		                        }
+		                    }
+		        				}
+		        		}
+		        	}
+					}
 	        System.out.println("=====================================Result of L4==================================");
 			for(int oz = 1 ; oz < item_tab ; oz++) {
 	        	for(int oy = 1 ; oy < item_tab ; oy++) {
